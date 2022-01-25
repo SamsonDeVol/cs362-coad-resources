5cf1a439523c4851927069f0162f1715d6ef2ac4
Micah Stalberg <stalberm@oregonstate.edu>
2022-01-25 10:30:27 -0800

    Create unit test for Region attributes References #8

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Region, type: :model do
     it "exists" do
         Region.new
     end
+    
+    describe "attributes" do
+        it "has a name" do
+            region = Region.new
+            expect(region).to respond_to(:name)
+        end
+    end
 end
