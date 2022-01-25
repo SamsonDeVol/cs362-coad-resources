0478878854affe08345934f9b231d20028c3c2d3
SamsonDeVol <devols@oregonstate.edu>
2022-01-25 10:32:32 -0800

    add unit test for region associations References #8

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Region, type: :model do
     it "exists" do
         Region.new
     end
+
+    describe "associations" do 
+      it "has many tickets" do 
+        expect(Region.new).to have_many(:tickets)
+      end
+    end
+
 end
