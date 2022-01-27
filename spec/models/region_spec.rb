24a5979aee63b83c44598ddffa838527ae2889a0
Micah Stalberg <stalberm@oregonstate.edu>
2022-01-27 11:31:27 -0800

    Add unit test for name length validation

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -7,14 +7,13 @@ RSpec.describe Region, type: :model do
 
     describe "attributes" do
       it "has a name" do
-          region = Region.new
-          expect(region).to respond_to(:name)
+          Region.new.should respond_to(:name)
       end
     end
 
     describe "associations" do 
       it "has many tickets" do 
-        expect(Region.new).to have_many(:tickets)
+        Region.new.should have_many(:tickets)
       end
     end
 
@@ -22,23 +21,28 @@ RSpec.describe Region, type: :model do
       it "returns the name" do
         name = "Deschutes"
       region = Region.new(name: name)
-      expect(region.to_s).to eq(name)
+      region.to_s.should eq(name)
       end
     end
 
     describe "::unspecified" do
       it "returns a Region with the name 'Unspecified'" do
         region = Region.unspecified
-        expect(region.name).to eq('Unspecified')
+        region.name.should eq('Unspecified')
       end
     end
 
     describe "validations" do
       it "cannot have a blank name" do
         region = Region.new(name: 'The Dogg Pound')
-        expect(region).to be_valid
+        region.should be_valid
         region = Region.new(name: nil)
-        expect(region).to_not be_valid
+        region.should_not be_valid
       end
+
+      it "cannot have a name longer than 255 chars" do
+        Region.new.should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+      end    
+        
     end
 end
