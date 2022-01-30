a7dc708c8b65f0fa5842a2a9b561b43aa3b9daeb
SamsonDeVol <devols@oregonstate.edu>
2022-01-29 21:27:34 -0800

    change all instances of should to expect in region_spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Region, type: :model do
 
     describe "attributes" do
       it "has a name" do
-          Region.new.should respond_to(:name)
+          expect(Region.new).to respond_to(:name)
       end
     end
 
     describe "associations" do 
       it "has many tickets" do 
-        Region.new.should have_many(:tickets)
+        expect(Region.new).to have_many(:tickets)
       end
     end
 
@@ -18,36 +18,36 @@ RSpec.describe Region, type: :model do
       it "returns the name" do
         name = "Deschutes"
       region = Region.new(name: name)
-      region.to_s.should eq(name)
+      expect(region.to_s).to eq(name)
       end
     end
 
     describe "::unspecified" do
       it "returns a Region with the name 'Unspecified'" do
         region = Region.unspecified
-        region.name.should eq('Unspecified')
+        expect(region.name).to eq('Unspecified')
       end
     end
 
     describe "validations" do
       it "cannot have a blank name" do
         region = Region.new(name: 'The Dogg Pound')
-        region.should be_valid
+        expect(region).to be_valid
         region = Region.new(name: nil)
-        region.should_not be_valid
+        expect(region).to_not be_valid
       end
 
       it "cannot have a name longer than 255 chars" do
-        Region.new.should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+        expect(Region.new).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
       end    
 
       it "must have a unique name" do
         duplicate_name = "Dogg Pound"
         region = Region.create!(name: duplicate_name)
         duplicate_region = Region.new(name: "Kurupt")
-        duplicate_region.should be_valid
+        expect(duplicate_region).to be_valid
         duplicate_region.name = duplicate_name
-        duplicate_region.should_not be_valid    
+        expect(duplicate_region).to_not be_valid    
       end
       
     end
