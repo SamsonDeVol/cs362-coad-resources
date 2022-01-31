d5dcae78076cdfd8508df98311711ebcf4cf9371
Micah Stalberg <stalberm@oregonstate.edu>
2022-01-30 19:32:06 -0800

    Add unit test for ResourceCategory name attribute References #23

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-    it "exists" do
-        ResourceCategory.new
-    end
-
+    describe "attributes" do
+        it "has a name" do
+            expect(ResourceCategory.new).to respond_to(:name)
+        end
+      end
 end
