9d86d2815a74fcedb37b677ae6c5b9f3eb4781df
Micah Stalberg <stalberm@oregonstate.edu>
2022-01-30 19:48:28 -0800

    Add unit test for ResourceCategory tickets association References #24

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe ResourceCategory, type: :model do
         it "has and belongs to many organizations" do
             expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
         end
+
+        it "has many tickets" do
+            expect(ResourceCategory.new).to have_many(:tickets)
+        end
+
     end
 end
