027382b66bb3a4b84fd4f07116f0fb00d353f3c2
SamsonDeVol <devols@oregonstate.edu>
2022-02-01 14:21:32 -0800

    Add unit test for #activate method in resource category References #28

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -3,44 +3,52 @@ require 'rails_helper'
 RSpec.describe ResourceCategory, type: :model do
     
     describe "attributes" do
-        it "has a name" do
-            expect(ResourceCategory.new).to respond_to(:name)
-        end
+      it "has a name" do
+          expect(ResourceCategory.new).to respond_to(:name)
+      end
 
-        it "has a boolean, active" do
-            expect(ResourceCategory.new).to respond_to(:active)
-        end
+      it "has a boolean, active" do
+          expect(ResourceCategory.new).to respond_to(:active)
+      end
     end
     
     describe "associations" do
-        it "has and belongs to many organizations" do
-            expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
-        end
+      it "has and belongs to many organizations" do
+        expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
+      end
 
-        it "has many tickets" do
-            expect(ResourceCategory.new).to have_many(:tickets)
-        end
+      it "has many tickets" do
+        expect(ResourceCategory.new).to have_many(:tickets)
+      end
     end
 
     describe "validations" do
-        it "cannot have a blank name" do
-            resCat = ResourceCategory.new(name: 'The Dogg Pound')
-            expect(resCat).to be_valid
-            resCat = ResourceCategory.new(name: nil)
-            expect(resCat).to_not be_valid
-        end
+      it "cannot have a blank name" do
+        resCat = ResourceCategory.new(name: 'The Dogg Pound')
+        expect(resCat).to be_valid
+        resCat = ResourceCategory.new(name: nil)
+        expect(resCat).to_not be_valid
+      end
 
-        it "cannot have a name longer than 255 chars" do
-            expect(ResourceCategory.new).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-        end
+      it "cannot have a name longer than 255 chars" do
+        expect(ResourceCategory.new).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+      end
 
-        it "must have a unique name" do
-            duplicate_name = "Westside Connection"
-            resCat = ResourceCategory.create!(name: duplicate_name)
-            duplicate_resCat = ResourceCategory.new(name: "WC")
-            expect(duplicate_resCat).to be_valid
-            duplicate_resCat.name = duplicate_name
-            expect(duplicate_resCat).to_not be_valid    
-        end
+      it "must have a unique name" do
+        duplicate_name = "Westside Connection"
+        resCat = ResourceCategory.create!(name: duplicate_name)
+        duplicate_resCat = ResourceCategory.new(name: "WC")
+        expect(duplicate_resCat).to be_valid
+        duplicate_resCat.name = duplicate_name
+        expect(duplicate_resCat).to_not be_valid    
+      end
+    end
+
+    describe "#activate" do
+      it "must set active value to true" do
+        resource_category = ResourceCategory.new(active: false)
+        resource_category.activate
+        expect(resource_category.active).to eq(true)
+      end
     end
 end
