661fbe149b0149e61fe5bb99aec4b46d29f98f81
SamsonDeVol <devols@oregonstate.edu>
2022-02-01 15:32:44 -0800

    Create factory for organization and add to let block in organization_spec.rb References #26

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,20 +1,8 @@
 require 'rails_helper'
 RSpec.describe Organization, type: :model do
 
-  let (:organization) {
-    Organization.new(name: "fake name",
-      status: 1, 
-      phone: "1111111111",
-      email: 'fake@email.com',
-      description: "describe it",
-      rejection_reason: "reason",
-      liability_insurance: false,
-      primary_name: "prime_name",
-      secondary_name: "sec_name",
-      secondary_phone: "2222222222",
-      title: "atitle",
-      transportation: 1)
-  }
+  let (:organization) { build_stubbed(:organization) }
+  
 
   describe "attributes" do
     it "has a name" do
