4ad2de4d64737db8d04443a8aa3c802ce96ec796
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-04 10:49:46 -0800

    Add unit test for Ticket name attribute References #32

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  describe "attributes" do
+    it "has a name" do
+      ticket = Ticket.new
+      expect(ticket).to respond_to(:name)
+    end
+  end
 end
