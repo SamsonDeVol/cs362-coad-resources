8b9b2a3fb573262a623fa442e4f6d1603912347d
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-06 11:00:12 -0800

    Add unit test for ticket description attribute References #32

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe Ticket, type: :model do
     it "has a name" do
       expect(ticket).to respond_to(:name)
     end
+
+    it "has a description" do
+        expect(ticket).to respond_to(:description)
+    end
   end
 end
