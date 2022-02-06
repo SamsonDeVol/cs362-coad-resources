4a3914e9e8430bded4b2a2bd1b27cf37839d1393
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-06 13:04:06 -0800

    Add unit test for validation of description length in Ticket References #34

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -53,5 +53,9 @@ RSpec.describe Ticket, type: :model do
     it "cannot have a name longer than 255 chars" do
       expect(ticket).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
     end
+
+    it "cannot have a name longer than 255 chars" do
+      expect(ticket).to validate_length_of(:description).is_at_most(1020).on(:create)
+    end
   end
 end
