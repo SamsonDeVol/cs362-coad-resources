ce7328c22c91b1ef4727dc429afdac275d1704e8
SamsonDeVol <devols@oregonstate.edu>
2022-02-22 10:48:27 -0800

    Write controller test for non logged in user index method References #45

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  describe "a non logged in user" do
+
+    it "redirects to the sign in scrreen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path);
+    end
+  end
+
 end
