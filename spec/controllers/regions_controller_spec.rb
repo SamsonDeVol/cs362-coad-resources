90e42bfa3c89349422e488e039d55725e7bead6e
SamsonDeVol <devols@oregonstate.edu>
2022-02-22 11:29:36 -0800

    Write controller test for non logged in user new method References #45

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -6,10 +6,24 @@ RSpec.describe RegionsController, type: :controller do
 
     it "redirects to the sign in scrreen" do
       get :index
-      expect(response).to redirect_to(new_user_session_path);
+      expect(response).to redirect_to(new_user_session_path)
       get :show, params: {id: 'FAKE'}
-      expect(response).to redirect_to(new_user_session_path);
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
+#   describe "an organization user" do
+#     before do
+#       allow(controller).to receive(:current_user).and_return(create(:user, :organization))
+#       allow(controller).to receive(:user_signed_in?).and_return(true)
+#     end
+
+#     it "redirects to the sign in screen" do
+#       sign_in(create(:user)).
+#       get :index
+#       expect(response).to redirect_to(dashboard_path)
+#     end
+#   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do 
   factory :user do
     email { "fake@email.com" }
+    password { "fakepassword" }
     role { 1 }
   end
 end
