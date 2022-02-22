8f1dac2f2c898a9ad6e918774eed41b3bf9f49c9
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-22 11:52:13 -0800

    Add resource category controller test for non logged in person index method Reference #47

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+  describe "not loggined in person" do
+    it "redirects to log in page" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    
+  end
 end
