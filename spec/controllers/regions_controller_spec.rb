492d66f83da777e35db940d2b5a23a7ee306376c
SamsonDeVol <devols@oregonstate.edu>
2022-02-22 12:20:04 -0800

    Write region controller tests for non logged in user edit, update, destroy References #45

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
-  describe "a non logged in user" do
+  describe "a non logged in person" do
 
-    it "redirects to the sign in scrreen" do
+    it "redirects to the sign in screen" do
       get :index
       expect(response).to redirect_to(new_user_session_path)
       get :show, params: {id: 'FAKE'}
@@ -14,6 +14,12 @@ RSpec.describe RegionsController, type: :controller do
       post :create
       expect(response).to redirect_to(new_user_session_path)
       get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      delete :destroy, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
     end
   end
