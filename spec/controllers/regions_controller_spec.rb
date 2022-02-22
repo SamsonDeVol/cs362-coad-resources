0b155aa12978991ebe3f0562ad216166562c829c
SamsonDeVol <devols@oregonstate.edu>
2022-02-22 11:00:09 -0800

    Write controller test for non logged in user show method References #45

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe RegionsController, type: :controller do
     it "redirects to the sign in scrreen" do
       get :index
       expect(response).to redirect_to(new_user_session_path);
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path);
     end
   end
 
