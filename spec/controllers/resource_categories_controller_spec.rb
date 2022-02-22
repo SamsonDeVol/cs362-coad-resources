872a7ae9f13702cae3270dc6fe2321dfba007643
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-22 12:05:50 -0800

    Add resource category controller test for non logged in person new method Reference #47

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       get :show, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
