d10358e1ef77775d3efc6277c4194c317ffcb820
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-23 16:58:31 -0800

    Add organization controller test for non logged in person update method Reference #48

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe OrganizationsController, type: :controller do
             expect(response).to redirect_to(new_user_session_path)
             get :edit, params: {id: 'FAKE'}
             expect(response).to redirect_to(new_user_session_path)
+            patch :update, params: {id: 'FAKE'}
+            expect(response).to redirect_to(new_user_session_path)
+            put :update, params: {id: 'FAKE'}
+            expect(response).to redirect_to(new_user_session_path)
         end
     end
 end
