c8dd1fdbbdb2730458e5e0172be2648329ae846e
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-24 11:41:42 -0800

    Add resource category controller test for organization user index method Reference #47

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -26,5 +26,14 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
     end
   end
-  
+  describe "an organization user" do
+    it "redirects to the dashboard" do
+      organization_user = create(:user)
+      organization_user.confirm
+      sign_in(organization_user)
+
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@email.com" }
     password { "fakepassword" }
-    
+    role { "organization" }
   end
 end
