13d4a1e675b9debc42822d817da48f183a39a53b
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-23 16:08:07 -0800

    Add resource category controller test for non logged in person destroy method Reference #47

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -22,6 +22,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       patch :deactivate, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
