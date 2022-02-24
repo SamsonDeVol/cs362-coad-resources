484cadbbb7c321c7f03c93bf0c82ac8f98514936
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-24 12:13:27 -0800

    Add regions controller test for organization user create method Reference #45

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -36,6 +36,8 @@ RSpec.describe RegionsController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       get :new
       expect(response).to redirect_to(dashboard_path)
+      post :create
+      expect(response).to redirect_to(dashboard_path)
     end
   end
 end
