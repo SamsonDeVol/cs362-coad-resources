0d1ac7a6b5163c3e412bfe402f10b70dbfb64e11
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-24 11:47:57 -0800

    Add resource category controller test for organization user update method Reference #47

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -42,6 +42,10 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       get :edit, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
     end
   end
 end
