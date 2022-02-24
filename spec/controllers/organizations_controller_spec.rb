da4729e5553e474e207fec1c022df0aec52b7998
Micah Stalberg <stalberm@oregonstate.edu>
2022-02-23 16:49:45 -0800

    Add organization controller test for non logged in person new method Reference #48

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe OrganizationsController, type: :controller do
     
     describe "a non logged in person" do
-        
+
         it "redirects to the sign in screen" do
             get :index
+            expect(response).to redirect_to(new_user_session_path)
+            get :new
             expect(response).to redirect_to(new_user_session_path)
         end
     end
