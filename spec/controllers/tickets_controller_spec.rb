d641676a3a405d28c7e7c44c6e1e878095f5a712
Micah Stalberg <stalberm@oregonstate.edu>
2022-03-01 15:58:00 -0800

    Add tickets controller test for non logged in person show method References #49

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe TicketsController, type: :controller do
 
     describe "a non logged in person" do
-
-        it "can access new ticket page" do
-          get :new
-          expect(response).to be_successful
+        it "redirects to dashboard" do
+          get :show, params: {id: 'FAKE'}
+          expect(response).to redirect_to(dashboard_path)
+          # post :capture, params: {id: 'FAKE'}
+          # post :release, params: {id: 'FAKE'}
+          # patch :close, params: {id: 'FAKE'}
+          # delete :destroy, params: {id: 'FAKE'}
         end
       end
-
 end
