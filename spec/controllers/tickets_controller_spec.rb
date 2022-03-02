75486e5cc9a03db4acd58bf0e5065f6e6546f679
Micah Stalberg <stalberm@oregonstate.edu>
2022-03-01 16:02:53 -0800

    Add tickets controller test for non logged in person release method References #49

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe TicketsController, type: :controller do
           expect(response).to redirect_to(dashboard_path)
           post :capture, params: {id: 'FAKE'}
           expect(response).to redirect_to(dashboard_path)
-          # post :release, params: {id: 'FAKE'}
+          post :release, params: {id: 'FAKE'}
+          expect(response).to redirect_to(dashboard_path)
           # patch :close, params: {id: 'FAKE'}
           # delete :destroy, params: {id: 'FAKE'}
         end
