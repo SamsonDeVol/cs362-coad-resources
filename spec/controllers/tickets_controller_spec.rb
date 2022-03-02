16c43ba394cd7defe6ecb0a26708ac89d71f7a87
Micah Stalberg <stalberm@oregonstate.edu>
2022-03-01 16:31:56 -0800

    Add tickets controller test for unapproved organization user destroy method References #49

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -45,6 +45,8 @@ RSpec.describe TicketsController, type: :controller do
         expect(response).to redirect_to(dashboard_path)
         patch :close, params: {id: 'FAKE'}
         expect(response).to redirect_to(dashboard_path)
+        delete :destroy, params: {id: 'FAKE'}
+        expect(response).to redirect_to(dashboard_path)
       end
     end
 end
