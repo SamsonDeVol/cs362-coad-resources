c2d6309d0dd601b5727b20a4fea2b7551158f7dc
SamsonDeVol <devols@oregonstate.edu>
2022-03-03 11:23:28 -0800

    Add feature test for admin failing to create new region with invalid attributes References #52

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,22 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
-    describe "create new region" do
-        before :each do
-            admin_user = create(:user)
-            admin_user.confirm
-            admin_user.role = 'admin'
-            admin_user.save!
-            log_in_as(admin_user)
-        end
-        it "create new region as admin" do
-            visit '/regions/new'
-            fill_in 'Name', with: "Fake New Region"
-            click_on 'Add Region'
-            expect(page).to have_text "Region successfully created"
-            expect(page).to have_text "Fake New Region"
-        end
+    scenario "with valid attributes suceeds" do
+        admin_user = create(:user)
+        admin_user.confirm
+        admin_user.role = 'admin'
+        admin_user.save!
+        log_in_as(admin_user)
+        visit '/regions/new'
+        fill_in 'Name', with: "Fake New Region"
+        click_on 'Add Region'
+        expect(page).to have_text "Region successfully created"
+        expect(page).to have_text "Fake New Region"
     end
 
-
+    scenario "with invalid attribtes shows an error message" do 
+        admin_user = create(:user)
+        admin_user.confirm
+        admin_user.role = 'admin'
+        admin_user.save!
+        log_in_as(admin_user)
+        visit '/regions/new'
+        fill_in 'Name', with: ''
+        click_on 'Add Region'
+        expect(page).to have_text 'errors prohibited this region'
+    end
 end
