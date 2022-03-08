a5a77c6f9c7c4935c11e7993b7f7887cffede7ae
SamsonDeVol <devols@oregonstate.edu>
2022-03-08 12:59:54 -0800

    Add feature test for successfully creating an organization References #55

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
+  scenario "with valid attributes is successful" do
+    new_user = create(:user)
+    new_user.organization = nil
+    new_user.confirm
+    # admin user needed for email sent when click_on 'Apply'
+    admin_user = create(:admin_user)
+    admin_user.confirm
+    log_in_as(new_user)
+    visit '/new_organization_application'
+    expect(page).to have_text "Partner Organization Application"
+    choose 'organization_liability_insurance_true'
+    choose 'organization_agreement_one_true'
+    choose 'organization_agreement_two_true'
+    choose 'organization_agreement_three_true'
+    choose 'organization_agreement_four_true'
+    choose 'organization_agreement_five_true'
+    choose 'organization_agreement_six_true'
+    choose 'organization_agreement_seven_true'
+    choose 'organization_agreement_eight_true'
+    fill_in 'What is your name? (Last, First)', with: 'Fake, Name'
+    fill_in 'Organization Name', with: 'FakeOrg'
+    fill_in 'What is your title? (if applicable)', with: 'Fake Title'
+    fill_in 'What is your direct phone number? Cell phone is best.', with: '11111111111'
+    fill_in "Who may we contact regarding your organization's application if we are unable to reach you?", with: 'Fake Mom 1'
+    fill_in 'What is a good secondary phone number we may reach your organization at?', with: '222222222222'
+    fill_in "What is your Organization's email?", with: 'fakeorganizationemail@email.com'
+    choose 'organization_transportation_yes'
+    click_on 'Apply'
+    expect(page).to have_text 'Application Submitted'
+    
+  end
 end
