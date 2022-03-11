2b0b9815ac5c216162ca3ab1cac0b6ed6f4a6dfa
Micah Stalberg <stalberm@oregonstate.edu>
2022-03-10 21:07:11 -0800

    Add system test for organization user capturing a ticket References #60

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+    scenario "is successful" do
+        organization_user = create(:user)
+        organization_user.confirm
+        organization_user.organization.approve
+        organization_user.organization.save!
+        log_in_as(organization_user)
+        ticket = create(:ticket)
+        visit "/tickets/#{ticket.id}"
+        expect(page).to have_text "#{ticket.name}"
+        click_on 'Capture'
+        visit "/tickets/#{ticket.id}"
+        expect(page).to have_text "#{organization_user.organization.name}"
+        expect(page).to have_text "Release"
+    end
 end
