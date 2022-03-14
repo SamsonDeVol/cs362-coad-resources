b6b5639596e5f6f5f72ae5daa8ead1a5ba964407
SamsonDeVol <devols@oregonstate.edu>
2022-03-13 19:58:06 -0700

    Add unit test for dashboard helper method #dashboard_for References #68

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -12,4 +12,26 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
 
+  let (:user) { create(:user) }
+
+  describe "#dashboard_for" do
+    it "sends admin user to admin dashboard" do
+      admin_user = create(:admin_user)
+      expect(dashboard_for(admin_user)).to eq('admin_dashboard')
+    end
+    it "sends submitted organization user to submitted organization dashboard" do
+      puts(user.organization&.submitted?)
+      expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+    end
+    it "sends approved organization user to approved organization dashboard" do
+      user.organization.status = 0
+      expect(dashboard_for(user)).to eq('organization_approved_dashboard')
+    end
+    it "sends non approved or submitted organization user to create application dashboard" do
+      user.organization.status = 2
+      expect(dashboard_for(user)).to eq('create_application_dashboard')
+      user.organization.status = 3
+      expect(dashboard_for(user)).to eq('create_application_dashboard')
+    end
+  end
 end
